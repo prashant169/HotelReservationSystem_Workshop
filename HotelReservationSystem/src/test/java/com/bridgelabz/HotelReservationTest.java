5590096693635d757367d6c43e255e3bcec74cea
prashant169 <prashantphad2018@gmail.com>
2021-11-01 07:10:36 -0800

    Program for Add hotel name in reservation System

--- HotelReservationSystem/src/test/java/com/bridgelabz/HotelReservationTest.java
+++ HotelReservationSystem/src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -7,20 +8,21 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+
 public class HotelReservationTest {
 
-	@Test
-	public void whenHotelAdded_ToSystem_ShouldGetAdded() {
-		Hotel hotel1 = new Hotel("Lakeewood", 110, 90, 80, 80, 3);
-		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
-		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
-		Hotel[] hotelList = { hotel1, hotel2, hotel3 };
-		List<Hotel> hotels = Arrays.asList(hotelList);
-		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.add(hotel1);
-		hotelReservation.add(hotel2);
-		hotelReservation.add(hotel3);
-		List<Hotel> result = hotelReservation.getHotelList();
-		assertEquals(hotels, result);
-	}
+    @Test
+    public void whenHotelAdded_ToSystem_ShouldGetAdded() {
+        Hotel hotel1 = new Hotel("Lakeewood", 110, 90, 80, 80, 3);
+        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+        Hotel[] hotelList = {hotel1, hotel2, hotel3};
+        List<Hotel> hotels = Arrays.asList(hotelList);
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add(hotel1);
+        hotelReservation.add(hotel2);
+        hotelReservation.add(hotel3);
+        List<Hotel> result = hotelReservation.getHotelList();
+        assertEquals(hotels, result);
+    }
 }
