f5a345d48ae04c29941d67cd16fc0202f2fe5cef
prashant169 <prashantphad2018@gmail.com>
2021-11-01 07:09:42 -0800

    Program for Add hotel name in reservation system

---
+++ HotelReservationSystem/src/main/java/com/bridgelabz/HotelReservation.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservation {
+	private List<Hotel> hotels;
+
+	public HotelReservation() {
+		this.hotels = new ArrayList<Hotel>();
+	}
+
+	public void add(Hotel hotel) {
+		this.hotels.add(hotel);
+	}
+
+	public List<Hotel> getHotelList() {
+		return this.hotels;
+	}
+}
