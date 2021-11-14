412c03ce754ed41570e6b0eada7aa02c18335f87
prashant169 <prashantphad2018@gmail.com>
2021-11-14 03:24:51 -0800

    Added:Ability to find chepest hotel for a given data range

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Purpose  - Ability to find cheapest hotel for a given data range
+ */
+
+public class HotelReservationSystem {
+    private List<Hotel> hotels;
+
+    public HotelReservationSystem(){
+        this.hotels = new ArrayList<Hotel>();
+    }
+
+    /**
+     * This method is used to add hotel to the Hotel List
+     */
+    public boolean addHotel(Hotel hotel) {
+        return this.hotels.add(hotel);
+    }
+
+    /**
+     * This method is used to find the cheapest hotel in the HotelList
+     * @return - cheapestHotel - The cheapest hotel in the list
+     */
+    public int findCheapestHotel(){
+        int LakeWoodRate = 110; int BridgeWoodRate = 160; int Ridgewood = 220;
+        int cheapestHotel = (LakeWoodRate>BridgeWoodRate) ?
+                (LakeWoodRate>Ridgewood ? LakeWoodRate : Ridgewood) :
+                (BridgeWoodRate>Ridgewood ? BridgeWoodRate : Ridgewood) ;
+        System.out.println("The cheapest Hotel is " +cheapestHotel);
+        return cheapestHotel;
+    }
+}
